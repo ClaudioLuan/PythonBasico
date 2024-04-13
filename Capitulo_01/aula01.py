8c664ead3c0f8605e38b240f78180a3e7064c2a9
PythonBasico <prof.claudioluan@gmail.com>
2024-04-13 15:42:07 -0300

    update

---
+++ Capitulo_01/aula01.py
@@ -0,0 +1,11 @@
+# Aula 01 - Apresentar uma Mensagem no Python.
+# Objetivo: Ensinar como apresentar uma mensagem no Python.
+#           Aprender a função print no Python.
+
+
+# Utilizamos a função print no Python para apresentar uma mensagem.
+# Ela é utilizada para emitir uma informação de saída padrão. 
+# Exemplo: mostra um texto na tela, soma de valores e etc. 
+
+print("Olá Mundo!")
+print(1+2)
